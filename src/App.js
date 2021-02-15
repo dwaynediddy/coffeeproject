e84264479289f39615ef4e428c22e23010853b34
dwaynediddy <burrows.dwayne@icloud.com>
2021-02-15 22:34:22 +1000

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router} from 'react-router-dom'
+
+import { createBrowserHistory } from "history";
+
+// Enable Browser History for Back and Forward Button
+export const appHistory = createBrowserHistory();
 
 function App() {
   return (
     <div className="App">
+      <Router history={appHistory} />   
       <h2>Livs bitch ass cawfee shop</h2>
     </div>
   );
