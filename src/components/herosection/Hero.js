852500cdaa038084beb93c9ff6a1fb80771123de
dwaynediddy <burrows.dwayne@icloud.com>
2021-02-23 11:37:04 +0100

    trying smaller video because load time is a problem

--- src/components/herosection/Hero.js
+++ src/components/herosection/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import video from './video.mp4'
+import video from '././video/video.mp4'
 import { HeroContainer, HeroBg, VideoBg} from './HeroElement'
 
 const HeroSection = () => {
