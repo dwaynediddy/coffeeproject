0a1fe0bb3dd4ceb5179d12be03e94c218b04e1c2
dwaynediddy <burrows.dwayne@icloud.com>
2021-02-23 11:25:10 +0100

    video added

---
+++ src/components/herosection/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import video from './video.mp4'
+import { HeroContainer, HeroBg, VideoBg} from './HeroElement'
+
+const HeroSection = () => {
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={video} type="video/mp4" />
+            </HeroBg>
+            
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
