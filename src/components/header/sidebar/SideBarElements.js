4e65a213bfcdcb19bbb911bbd74d3fa575acecae
dwaynediddy <burrows.dwayne@icloud.com>
2021-02-25 17:59:05 +0100

    further bug fixes in styling

--- src/components/header/sidebar/SideBarElements.js
+++ src/components/header/sidebar/SideBarElements.js
@@ -38,7 +38,7 @@ color: #fff;
 
 export const SidebarMenu = styled.ul`
 display: grid;
-grid-template-colums: 1fr;
+grid-template-columns: 1fr;
 grid-template-rows: repeat(6, 80px);
 text-align: center;
 
@@ -53,7 +53,7 @@ align-items: center;
 justify-content: center;
 font-size: 1.5rem; 
 text-decoration: none;
-list-stye: none;
+list-style: none;
 transition: 0.2s ease-in-out;
 text-decoration: none;
 color: #fff;
