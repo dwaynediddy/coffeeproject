e224152a44b1d68fe0c13654034fafb5bb0f4a41
dwaynediddy <burrows.dwayne@icloud.com>
2021-03-02 15:46:36 +0100

    trying to add commerce.js to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Landing from './screens/Landing'
 import Product from '../src/components/products/Products'
 
+import { commerce } from '../src/components/commerce/commerce'
+
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const { data }= await commerce.products.list()
+
+    setProducts(data)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
     <div className="App">
       <Router>
