e3aa03db66ddc76da065c9989daf950e7029f4e4
dwaynediddy <burrows.dwayne@icloud.com>
2021-03-03 19:06:57 +0100

    adding items feature added

--- src/store/src/StoreApp.js
+++ src/store/src/StoreApp.js
@@ -7,7 +7,7 @@ import Navbar from './components/navbar/Navbar'
 
 function StoreApp() {
   const [ products, setProdcuts ] = useState([])
-  // const [cart, setCart] = useState({})
+  const [cart, setCart] = useState({})
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list()
@@ -15,12 +15,29 @@ function StoreApp() {
     setProdcuts(data)
   }
 
-  useEffect(() => fetchProducts(), [])
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+
+    
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity)
+
+    setCart(item.cart)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+    fetchCart()
+  }, [])
+
+  console.log(cart)
 
   return (
     <div className="App">
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 }
--- src/store/src/components/navbar/Navbar.js
+++ src/store/src/components/navbar/Navbar.js
@@ -12,7 +12,7 @@ import { ShoppingCart } from '@material-ui/icons'
 import logo from '../../assests/store.png'
 import useStyles from './styles'
 
-const Navbar = () => {
+const Navbar = ({ totalItems}) => {
     const classes = useStyles()
     return (
         <>
@@ -25,7 +25,7 @@ const Navbar = () => {
                     <div className={classes.grow}/>
                     <div className={classes.button}>
                         <IconButton aria-label='Show cart items' color='inherit'>
-                            <Badge badgeContent={2} color='secondary'>
+                            <Badge badgeContent={totalItems} color='secondary'>
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
