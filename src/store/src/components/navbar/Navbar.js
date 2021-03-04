c38db9d6d0640f9efd43ef50a8d2d38799476886
dwaynediddy <burrows.dwayne@icloud.com>
2021-03-04 21:39:02 +0100

    correcting logo route but bug in store nav cart not showing

--- src/store/src/components/navbar/Navbar.js
+++ src/store/src/components/navbar/Navbar.js
@@ -22,12 +22,12 @@ const Navbar = ({ totalItems}) => {
         <>
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
-                    <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
+                    <Typography component={Link} to='/StoreApp' variant='h6' className={classes.title} color='inherit'>
                         <img src={logo} alt='commerce.js' height='25px' className={classes.image} />
                         Livs store
                     </Typography>
                     <div className={classes.grow}/>
-                        {/* {location.pathname === '/' && ( */}
+                        {location.pathname === '/cart' && (
                             <div className={classes.button}>
                         <IconButton component={Link} to='/cart' aria-label='Show cart items' color='inherit'>
                             <Badge badgeContent={totalItems} color='secondary'>
@@ -35,7 +35,7 @@ const Navbar = ({ totalItems}) => {
                             </Badge>
                         </IconButton>
                     </div> 
-                     {/* )} */}
+                      )}
                 </Toolbar>
             </AppBar>
         </>
